2064ba72eddf7250fc03160dfe9533c9498de1f6
Hassan Gani <hassanganiu1@gmail.com>
2026-03-07 19:02:40 +0100

    fix(cache): revert HiveStore back to InMemoryStore for graphql cache

--- lib/src/core/talq_client.dart
+++ lib/src/core/talq_client.dart
@@ -147,7 +147,7 @@ class TalqClient {
     // Create client
     try {
       client = GraphQLClient(
-        cache: GraphQLCache(store: HiveStore()),
+        cache: GraphQLCache(store: InMemoryStore()),
         link: link,
         queryRequestTimeout: _receiveTimeout,
       );
